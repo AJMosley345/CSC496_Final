935b1031ffc6a43384da650ecbc442f1ce02baa1
Anthony Mosley <59812149+AJMosley345@users.noreply.github.com>
2023-12-14 14:15:33 -0500

    Committed physics and pokemon stuff

--- GameScene.swift
+++ GameScene.swift
@@ -20,27 +20,68 @@ class GameScene: SKScene {
     private var player: Player?
     
     override func sceneDidLoad() {
-
+        
         self.lastUpdateTime = 0
         physicsWorld.contactDelegate = self
-
+        
     }
     
     override func didMove(to view: SKView) {
         playBackgroundMusic("background_music.mp3")
         player = childNode(withName: "player") as? Player
+        
+        setupPlayerPhysics()
+        spawnPokemons()
     }
     func playBackgroundMusic(_ filename: String) {
-            let backgroundMusic = SKAudioNode(fileNamed: filename)
-            backgroundMusic.autoplayLooped = true
-            addChild(backgroundMusic)
-        }
+        let backgroundMusic = SKAudioNode(fileNamed: filename)
+        backgroundMusic.autoplayLooped = true
+        addChild(backgroundMusic)
+    }
     
     func playCaptureSound() {
         let playSound = SKAction.playSoundFileNamed("capture_sound.mp3", waitForCompletion: false)
         self.run(playSound)
     }
-
+    
+    func setupPlayerPhysics() {
+        player?.physicsBody = SKPhysicsBody(rectangleOf: player?.size ?? CGSize.zero)
+        player?.physicsBody?.categoryBitMask = PhysicsCategory.Player
+        player?.physicsBody?.contactTestBitMask = PhysicsCategory.Pokemon
+        player?.physicsBody?.collisionBitMask = PhysicsCategory.None
+        player?.physicsBody?.isDynamic = true
+    }
+    
+    func spawnPokemons() {
+        for _ in 1...10 {  // Example: Spawn 10 Pokémons, adjust the number as needed
+            spawnPokemon()
+        }
+    }
+    
+    func spawnPokemon() {
+        let pokemonNumber = Int.random(in: 1...151)
+        let pokemonNode = SKSpriteNode(imageNamed: "\(pokemonNumber)")
+        pokemonNode.name = "pokemon"
+        pokemonNode.position = CGPoint(x: CGFloat.random(in: 0...self.size.width),
+                                       y: CGFloat.random(in: 0...self.size.height))
+        pokemonNode.physicsBody = SKPhysicsBody(rectangleOf: pokemonNode.size)
+        pokemonNode.physicsBody?.categoryBitMask = PhysicsCategory.Pokemon
+        pokemonNode.physicsBody?.contactTestBitMask = PhysicsCategory.Player
+        pokemonNode.physicsBody?.collisionBitMask = PhysicsCategory.None
+        pokemonNode.physicsBody?.isDynamic = false
+        addChild(pokemonNode)
+        
+        pokemonNode.alpha = 0  // Make Pokémon initially invisible
+        addChild(pokemonNode)
+        
+    }
+    
+    
+    struct PhysicsCategory {
+        static let None: UInt32 = 0
+        static let Player: UInt32 = 0b1
+        static let Pokemon: UInt32 = 0b10
+    }
     
     
     func touchDown(atPoint pos : CGPoint) {
@@ -53,8 +94,6 @@ class GameScene: SKScene {
             }
         }
     }
-    
-    
     
     
     func touchMoved(toPoint pos : CGPoint) {
@@ -110,19 +149,50 @@ class GameScene: SKScene {
             entity.update(deltaTime: dt)
         }
         
+        // Check distance between each Pokémon and the player
+        enumerateChildNodes(withName: "pokemon") { node, _ in
+            guard let pokemonNode = node as? SKSpriteNode, let player = self.player else { return }
+            
+            let distance = hypot(pokemonNode.position.x - player.position.x,
+                                 pokemonNode.position.y - player.position.y)
+            
+            let visibilityThreshold: CGFloat = 100  // Adjust this value as needed
+            if distance < visibilityThreshold {
+                pokemonNode.alpha = 1  // Make Pokémon visible
+            } else {
+                pokemonNode.alpha = 0  // Keep Pokémon invisible
+            }
+        }
+        
         self.lastUpdateTime = currentTime
     }
 }
 
-extension GameScene:SKPhysicsContactDelegate{
+extension GameScene: SKPhysicsContactDelegate {
     func didBegin(_ contact: SKPhysicsContact) {
-        let firstNode = contact.bodyA.node as? SKSpriteNode
-        let secondNode = contact.bodyB.node as? SKSpriteNode
-        
-        if let firstNode = firstNode, let secondNode = secondNode {
-            if (firstNode.name == "player" && secondNode.name == "pokemon") || (firstNode.name == "pokemon" && secondNode.name == "player") {
-                print("player touched pokemon")
+        var firstBody: SKPhysicsBody
+        var secondBody: SKPhysicsBody
+
+        // Determine which body is the player and which is the Pokémon
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+
+        // Check if the contact is between the player and a Pokémon
+        if firstBody.categoryBitMask == PhysicsCategory.Player && secondBody.categoryBitMask == PhysicsCategory.Pokemon {
+            if let pokemonNode = secondBody.node as? SKSpriteNode {
+                // Here, handle the logic when a player contacts a Pokémon
+                print("Player has encountered a Pokémon!")
+                playCaptureSound()
+
+                // Example: remove the Pokémon from the scene
+                pokemonNode.removeFromParent()
             }
         }
     }
 }
+
